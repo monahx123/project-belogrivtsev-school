aa8d2e1eb9cc5485e7959d45bb15388ec11c57dc
monahx123 <dimabilogrivcev@gmail.com>
2024-04-08 23:27:54 +0300

    homework display none

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ $('.multiple-items').slick({
   prevArrow: '<button class="slider-box__btn slider-box__btnnext"><img src="img/next.svg" alt=""></button>',
   nextArrow: '<button class="slider-box__btn slider-box__btnprev"><img src="img/prev.svg" alt=""></button>',
 });
+
+$('.tab').on('click', function (e) {
+  e.preventDefault();
+
+  $($(this).siblings()).removeClass('tab--active');
+  $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+  $(this).addClass('tab--active');
+  $($(this).attr('href')).addClass('tabs-content--active');
+});
