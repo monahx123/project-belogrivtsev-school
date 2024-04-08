ee70a302f786aa4717bb3a582ce8199acdd7187c
monahx123 <149580117+monahx123@users.noreply.github.com>
2024-04-08 17:00:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.multiple-items').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<button class="slider-box__btn slider-box__btnnext"><img src="img/next.svg" alt=""></button>',
+  nextArrow: '<button class="slider-box__btn slider-box__btnprev"><img src="img/prev.svg" alt=""></button>',
+});
